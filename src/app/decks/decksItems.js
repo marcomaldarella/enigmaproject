4b13cda9f926499eadadbe9ee87d41388ea05bad
marcomaldarella <marcomaldarella@gmail.com>
2025-03-07 00:36:22 +0100

    ok let's go

--- src/app/decks/decksItems.js
+++ src/app/decks/decksItems.js
@@ -1,6 +1,6 @@
 export const decksItems = [
   {
     name: "Enigma Project Overview",
-    url: "/decks/deck-1", // Link a una pagina specifica o un file
+    url: "https://enigmaslides.netlify.app", // Link a una pagina specifica o un file
   }
 ];
